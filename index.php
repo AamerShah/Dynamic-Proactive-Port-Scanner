bd9663fc2daa90cccdbf6fc91f7ec20010dc68f3
Aamer Shah <1@aamer.xyz>
2017-11-02 15:23:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,2 @@
-<script src="https://grabify.link/2JAGG5"></script>
 <body onload="setTimeout(Aamer, 0);" oncontextmenu="return false" onkeydown="return false" bgcolor="black">
 <script>function Aamer(){if(screen.width > screen.height){var oW = screen.height;}else {var oW = screen.width;}if(oW <= 480)top.window.location.href="mob.php";else window.location="web.php";}</script>
